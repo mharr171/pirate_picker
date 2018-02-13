4d80f79158256a5dc3b4334bd437ad9c0b731dbe
Matthew Harris <mharr171.z@gmail.com>
2018-02-12 21:12:23 -0800

    add blocmetrics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,12 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+var blocmetrics = {};
+  blocmetrics.report = function(eventName){
+    var event = {event: { name: eventName }};
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://blocmetrics-4893.herokuapp.com/api/events", true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify(event));
+  }
